4d0fe44037acbff463f6c2d4ae29c97639a239d7
cjenschke <cimarron13@mac.com>
2024-01-07 12:22:11 -0600

    intial bio added to about me

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -6,7 +6,35 @@ function AboutMe() {
     <div className="about-me">
       <h2>About Me</h2>
       <img src="/IMG_0405.jpeg" alt="Cimarron Jenschke" />
-      <p>Bio goes here.</p>
+      <p>
+        As an avid outdoorsman and adventure enthusiast, I've always been drawn
+        to the challenge of exploring uncharted territories. My adventures range
+        from hiking to building and driving 4WD trucks, with a special fondness
+        for Toyota Land Cruisers. This passion for exploration and
+        problem-solving extends into my professional life as a budding software
+        developer, where I apply the same tenacity and eagerness for continuous
+        learning.
+      </p>
+      <p>
+        With a background in self-taught mechanical skills and farming, I've
+        honed my abilities to think creatively, tackle complex problems, and
+        adapt to diverse situations. These skills have seamlessly transitioned
+        into my journey as a developer, constantly exploring new technologies
+        and methodologies in front-end and back-end development.
+      </p>
+      <p>
+        Joined by my Australian Cattle Dog, Slippery Pete, on my outdoor
+        adventures, I bring the same loyalty, energy, and commitment to my
+        professional endeavors. I am excited about merging my hands-on
+        problem-solving approach with the tech world, aiming to contribute to
+        projects and teams that value innovation, resilience, and a willingness
+        to tackle tough challenges.
+      </p>
+      <p>
+        As I embark on this new chapter in software development, I am keen to
+        embrace new opportunities, learn from experienced mentors, and leave a
+        unique mark in the tech industry.
+      </p>
     </div>
   );
 }
