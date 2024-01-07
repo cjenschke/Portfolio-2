bce1304fb29d73b8345090aecc8b855dc99fe5ad
cjenschke <cimarron13@mac.com>
2024-01-07 12:42:25 -0600

    icon links added to footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,36 @@ function Footer() {
   return (
     <footer>
       <p>© 2024 Cimarron Jenschke</p>
-      {/* Add links to your social media profiles */}
+      <div className="social-media-links">
+        <a
+          href="https://github.com/cjenschke"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-github"></i>
+        </a>{' '}
+        <a
+          href="https://www.linkedin.com/in/cimarron-jenschke-a59ab5290/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-linkedin"></i>
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCoHLeKWqEnCRMY3WFYsAUGQ"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-youtube"></i>
+        </a>
+        <a
+          href="https://www.instagram.com/justanotherlandcruiser/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram"></i>
+        </a>
+      </div>
     </footer>
   );
 }
